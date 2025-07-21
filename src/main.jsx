314a30f0e40d66d6165863a7940b9c229572a3e9
Lavishkamboj <151373968+Lavishkamboj@users.noreply.github.com>
2025-07-21 21:48:56 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { AuthProvider } from '../auth.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import { AuthProvider } from '../auth.jsx';
 
 createRoot(document.getElementById('root')).render(
-
-<AuthProvider>
+  <StrictMode>
+    <AuthProvider>
       <App />
     </AuthProvider>
- 
-)
+  </StrictMode>
+);
